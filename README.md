c342a23e77bcf47ffae852f7fe9b4e9d021952d8
Toyosatomimi no Miko <bryan.chen@uwaterloo.ca>
2025-01-25 21:51:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Mofubot
+
+## Overview
+A joke Discord bot, written in Java with JDA (Java Discord API).
+
+## Usage:
+To start the bot, use `./gradlew run`.
+
+To shut off the bot, write `shutdown` to standard input or `Ctrl + C`.
+
+## TODO:
+* Complete music functionality
---
+++ app/src/main/java/org/bot/utilities/ResponseHandler.java
@@ -0,0 +1,20 @@
+package org.bot.utilities;
+
+import java.util.Map;
+
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+
+public class ResponseHandler {
+    public static void handleMessage(String content, MessageChannel textChannel) {
+        System.out.println("Parsing for responses");
+        System.out.println("Message: " + content);
+        for (Map.Entry<String, String> entry: Responses.getInstance().getResponses().entrySet()) {
+            System.out.println("Checking response key: " + entry.getKey());
+            if (content.toLowerCase().contains(entry.getKey())) {
+                System.out.println("Match found! Sending: " + entry.getValue());
+                textChannel.sendMessage(entry.getValue()).queue();
+                break;
+            }
+        }
+    }
+}
