e8ca8a7056df06623a9794fa032b419dbc1fdea2
Toyosatomimi no Miko <bryan.chen@uwaterloo.ca>
2025-01-29 02:12:23 -0500

    Remove useless wrapper class

--- app/src/main/java/org/mofubot/utilities/ResponseHandler.java
+++ app/src/main/java/org/mofubot/utilities/ResponseHandler.java
@@ -1,14 +1,20 @@
 package org.mofubot.utilities;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 
 public class ResponseHandler {
+    private static final Map<String, String> RESPONSES = new HashMap<>() {{
+        put("mofumofu", "fuwafuwa!");
+        put("fuwafuwa", "mofumofu!");
+    }};
+
     public static void handleMessage(String content, MessageChannel textChannel) {
         System.out.println("Parsing for responses");
         System.out.println("Message: " + content);
-        for (Map.Entry<String, String> entry: Responses.getInstance().getResponses().entrySet()) {
+        for (Map.Entry<String, String> entry: RESPONSES.entrySet()) {
             System.out.println("Checking response key: " + entry.getKey());
             if (content.toLowerCase().contains(entry.getKey())) {
                 System.out.println("Match found! Sending: " + entry.getValue());
