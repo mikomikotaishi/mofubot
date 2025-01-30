86097c45327cdfeeed830fce893b8cb7b9f4af05
Toyosatomimi no Miko <bryan.chen@uwaterloo.ca>
2025-01-30 03:25:49 -0500

    hush hush

---
+++ app/src/main/java/org/mofubot/commands/imageboard/E621.java
@@ -0,0 +1,15 @@
+package org.mofubot.commands.imageboard;
+
+import javax.annotation.Nonnull;
+
+import org.mofubot.commands.structures.APICommand;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class E621 implements APICommand {
+    private E621() {}
+
+    public static void invoke(@Nonnull SlashCommandInteractionEvent event) {
+        
+    }
+}
---
+++ app/src/main/java/org/mofubot/commands/imageboard/Rule34.java
@@ -0,0 +1,15 @@
+package org.mofubot.commands.imageboard;
+
+import javax.annotation.Nonnull;
+
+import org.mofubot.commands.structures.APICommand;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class Rule34 implements APICommand {
+    private Rule34() {}
+
+    public static void invoke(@Nonnull SlashCommandInteractionEvent event) {
+        
+    }
+}
